f655e926bf5939e54aa087d20f432b9bb72e5fb8
aeinspanier <aeinspanier23@gmail.com>
2021-12-19 10:28:32 -0600

    C Rectangle Class Area Calculator

--- section2.cpp
+++ section2.cpp
@@ -1,15 +1,35 @@
 #include<iostream>
-#include<stdio.h>
+//#include<stdio.h>
+//#include<stdlib.h>
 using namespace std;
+
+struct Rectangle{
+    int length;
+    int breadth;
+
+
+void initialize(int l, int b)
+{
+    length = l;
+    breadth = b;
+}
+
+int area()
+{
+    return length*breadth;
+}
+};
 
 int main()
 {
-    int a=10;
-    int *p;
-    p=&a;
+    Rectangle r;
+    int l,b;
+    printf("enter length & Breadth");
+    cin>>l>>b;
 
-    printf("using pointer, %d",*p);
+    r.initialize(l,b);
 
+    cout<<"area is  "<<r.area()<<endl;
     return 0;
 }
 
