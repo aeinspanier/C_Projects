32804b99e43fe5843bd9ba02e6e59cce1175a659
aeinspanier <aeinspanier23@gmail.com>
2021-12-21 07:26:55 -0600

    Adding functions to ArrayADT.cpp

--- ArrayADT.cpp
+++ ArrayADT.cpp
@@ -42,11 +42,91 @@ int Delete(struct Array *arr, int idx){
     return x;
 }
 
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y=temp;
+}
+
+int LinearSearch(struct Array *arr, int key){
+    for(int i=0;i<arr->length;i++){
+        if(key == arr->A[i]){
+            swap(&arr->A[i], &arr->A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+int RBinSearch(int a[], int l, int h, int key){
+    int mid;
+
+    if(l<=h){
+        mid = (l+h)/2;
+        if(key==a[mid]){
+            return mid;
+        }else if(key<a[mid]){
+            return RBinSearch(a,l,mid-1,key);
+        }else{
+            return RBinSearch(a,mid+1,h,key);
+        }
+    }
+    return -1;
+}
+
+int Get(struct Array arr, int idx){
+    if(idx >= 0 && idx<arr.length){
+        return arr.A[idx];
+    }
+    return -1;
+}
+
+void Set(struct Array *arr, int idx, int key){
+    if(idx>=0 && idx<arr->length){
+        arr->A[idx]=key;
+    }
+}
+
+int Max(struct Array arr){
+    int max=arr.A[0];
+
+    for(int i=1;i<arr.length;i++){
+        if(arr.A[i]>max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+
+int Min(struct Array arr){
+    int min=arr.A[0];
+
+    for(int i=1;i<arr.length;i++){
+        if(arr.A[i]<min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int Sum(struct Array arr){
+    int sum=0;
+
+    for(int i=0;i<arr.length;i++){
+        sum += arr.A[i];
+    }
+    return sum;
+}
+
+float Avg(struct Array arr){
+    return (float)Sum(arr)/arr.length;
+}
+
 int main(){
 
     //Initialize Array
-    struct Array arr={{2,3,4,5,6}, 10, 5};
-    printf("%d\n",Delete(&arr,4));
+    struct Array arr={{2,3,14,4,5,6}, 10, 5};
+    printf("%f\n", Avg(arr));
+    //Set(&arr, 0, 15);
     Display(arr);
 
 }
