30197db70038d1006d3c93bc795a004188525352
aeinspanier <aeinspanier23@gmail.com>
2021-12-24 10:09:12 -0600

    pushing LinkedList.c

--- ArrayADT.cpp
+++ ArrayADT.cpp
@@ -121,12 +121,165 @@ float Avg(struct Array arr){
     return (float)Sum(arr)/arr.length;
 }
 
-int main(){
+void Reverse(struct Array *arr){
+    int i,j;
+    for(i=0, j=arr->length-1;i<j;i++, j--){
+        swap(&arr->A[i],&arr->A[j]);
+    }
+}
 
-    //Initialize Array
-    struct Array arr={{2,3,14,4,5,6}, 10, 5};
-    printf("%f\n", Avg(arr));
-    //Set(&arr, 0, 15);
-    Display(arr);
+void InsertSort(struct Array *arr, int x){
+    int i=arr->length-1;
+    if(arr->length == arr->size)
+        return;
+
+    while(i>=0 && arr->A[i]>x){
+        arr->A[i+1] = arr->A[i];
+        i--;        
+    }
+    arr->A[i+1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr){
+    int i;
+    for(i=0;i<arr.length-1;i++){
+        if(arr.A[i] > arr.A[i+1])
+            return 0;
+    }
+    return 1;
+}
+
+void Rearrange(struct Array *arr){
+    int i,j;
+    i=0;
+    j=arr->length;
+
+    while(i<j){
+        while(arr->A[i]<0)i++;
+        while(arr->A[j]>=0)j--;
+        if(i<j) swap(&arr->A[i],&arr->A[j]);
+    }
+}
+
+struct Array * Merge(struct Array *arr1, struct Array *arr2){
+    int i, j, k;
+    i=j=k=0;
+    struct Array *arr3;
+    arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i] < arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+        }else{
+            arr3->A[k++] = arr2->A[j++];
+        }
+    }
+
+    for(;i<arr1->length;i++)
+        arr3->A[k++] = arr1->A[i];
+    for(;j<arr2->length;j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+    return arr3;
+}
+
+struct Array * Union(struct Array *arr1, struct Array *arr2){
+    int i, j, k;
+    i=j=k=0;
+    struct Array *arr3;
+    arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i] < arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+        }else if(arr1->A[i] == arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+        else{
+            arr3->A[k++] = arr2->A[j++];
+        }
+    }
+
+    for(;i<arr1->length;i++)
+        arr3->A[k++] = arr1->A[i];
+    for(;j<arr2->length;j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
+struct Array * Intersection(struct Array *arr1, struct Array *arr2){
+    int i, j, k;
+    i=j=k=0;
+    struct Array *arr3;
+    arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i] < arr2->A[j]){
+            i++;
+        }else if(arr1->A[i] == arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+        else{
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
+struct Array * Difference(struct Array *arr1, struct Array *arr2){
+    int i, j, k;
+    i=j=k=0;
+    struct Array *arr3;
+    arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i] < arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+        }else if(arr1->A[i] == arr2->A[j]){
+            i++;
+            j++;
+        }
+        else{
+            j++;
+        }
+    }
+
+    for(;i<arr1->length;i++)
+        arr3->A[k++] = arr1->A[i];
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
+int main(){
+    struct Array arr1;
+    int ch;
+
+    printf("Enter size of array \n");
+    scanf("%d\n",&arr1.size);
+    arr1.A = (int *)malloc(arr1.size*sizeof(int));
+
+    printf("Menu \n");
+    printf("1. Insert \n");
+    printf("2. Delete \n");
+    printf("3. Search \n");
+    printf("4. Sum \n");
+    printf("5. Display \n");
+    printf("6. Exit \n");
+
+    printf("Enter your choice \n");
+    scanf("%d\n",&ch);
 
 }
