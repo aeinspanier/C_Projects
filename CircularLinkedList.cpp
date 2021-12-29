3578140f5a735da57db699f227d83acedde75a0d
aeinspanier <aeinspanier23@gmail.com>
2021-12-29 15:11:42 -0600

    expanding circular linked list

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -16,6 +16,9 @@ class CircularLinkedList{
         void recursiveDisplay(Node *p);
         Node *getHead(){return head;}
         ~CircularLinkedList();
+        void Insert(int index, int x);
+        int Length();
+        int Delete(int index);
 };
 
 CircularLinkedList::CircularLinkedList(int *A, int n){
@@ -72,10 +75,81 @@ CircularLinkedList::~CircularLinkedList(){
     }
 }
 
+int CircularLinkedList::Length(){
+    int len = 0;
+    Node *p = head;
+
+    do{
+        len++;
+        p=p->next;
+    }while(p != head);
+    return len;
+}
+
+void CircularLinkedList::Insert(int index, int x){
+    Node *t, *p=head;
+
+    if(index<0 || index>Length()) return;
+
+    if(index == 0){
+        t = new Node;
+        t->data = x;
+        if(head == NULL){
+            head = t;
+            head->next = head;
+        }else{
+            while(p->next != head){
+                p=p->next;
+            }
+            p->next=t;
+            t->next = head;
+            head = t;
+        }
+    }else{
+        for(int i=0;i<index-1;i++){
+            p=p->next;
+        }
+        t= new Node;
+        t->data = x;
+        t->next = p->next;
+        p->next = t;
+    }
+}
+
+int CircularLinkedList::Delete(int index){
+    Node *q, *p = head;
+    int x;
+
+    if(index<0 || index > Length()) return -1;
+
+    if(index == 1){
+        while(p->next != head) p=p->next;
+        x = head->data;
+        if(head == p){
+            delete head;
+            head = nullptr;
+        }else{
+            p->next = head->next;
+            delete head;
+            head = p->next;
+        }
+    }else{
+        for(int i=0;i<index-2;i++){
+            p=p->next;
+        }
+        q=p->next;
+        p->next = q->next;
+        x=q->data;
+        delete q;
+    }
+    return x;
+}
+
 int main(){
     int A[]={1,3,5,7,9};
 
     CircularLinkedList cl(A,sizeof(A)/sizeof(A[0]));
+    cout<<cl.Delete(2)<<endl;
     cl.Display();
 
     Node *h = cl.getHead();
